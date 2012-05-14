9beb3f82eec312df2de2a147eaa5c37cd4aa940f
faitdivers <jsilva@ubuntu.(none)>
2012-05-14 11:54:26 +0100

    Tratamento de CTRL-C

--- src/client.c
+++ src/client.c
@@ -54,6 +54,8 @@ pthread_mutex_t fifo;
 fifo_node * front;
 fifo_node * back;
 int debug;
+pthread_t main_t;
+pthread_t wr_t;
 
 typedef struct main_clean_s 
 {
@@ -61,39 +63,85 @@ typedef struct main_clean_s
 	package ** tosend;
 }main_clean_s;
 
+typedef struct wr_clean_s 
+{
+	package ** to_recv;
+	item_client ** item;
+}wr_clean_s;
 
 typedef struct wr_arg_t
 {
 	int wr_arg;
-	pthread_t main;
 }wr_arg_t;
 
 
-void wr_clean(package * to_recv, fifo_node **front)
+void block_all_signals()
 {
-	free(to_recv);
-	FreeFifo(front);
+	sigset_t new_set;
+	sigfillset(&new_set);
+	sigprocmask(SIG_SETMASK, &new_set, NULL);	
 }
+
+void block_except_sigint()
+{
+	sigset_t new_set;
+	sigfillset(&new_set);
+	sigdelset(&new_set, SIGINT);
+	sigprocmask(SIG_SETMASK, &new_set, NULL);	
+}
+
+void wr_clean(void * arg)
+{
+	wr_clean_s * aux = (wr_clean_s *) arg;
+	free(*(aux->to_recv));
+	if(*(aux->item)!=NULL)
+	{
+		free(*(aux->item));
+	}
+	pthread_mutex_lock(&fifo);
+	FreeFifo(&front);
+	pthread_mutex_unlock(&fifo);
+}
+
+void sig_handler(){
+	printf("\nCTRL-C detected\n");
+	if(wr_t!=-1) /*If wr_t hasn't been created yet*/
+	{
+		pthread_cancel(wr_t);
+		pthread_join(wr_t,NULL);
+	}
+	pthread_cancel(main_t);
+}
+
 void * write_read ( void * arg){
 	int n;
 	int ntemp;
 	char ctemp;
 	int sockfd;
 	wr_arg_t * aux;
-	pthread_t main_t;
 	package * tosend, *torecv; 
 	item_client *item;
+	int old_cancel_type;
+	wr_clean_s aux_clean;
+	aux_clean.to_recv = &torecv;
+	aux_clean.item=&item;
+	
+	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS,&old_cancel_type);
+    pthread_cleanup_push(wr_clean,(void*)&aux_clean);
+	
 	
     torecv = (package*) malloc(sizeof(package));
     
     aux = (wr_arg_t*) arg;
     sockfd = aux->wr_arg;
-    main_t = aux->main;
+
 	while(1){
 		sem_wait(&fifo_cnt);
+		pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED,&old_cancel_type); /*Guarantees that the thread doesn't get canceled while holding the lock*/
 		pthread_mutex_lock(&fifo);
 		item = (item_client *) dequeue(&front, &back);
 		pthread_mutex_unlock(&fifo);
+		pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS,&old_cancel_type);
 		
 		tosend = &(item->tosend);
 		ctemp = tosend->op;
@@ -109,10 +157,7 @@ void * write_read ( void * arg){
 		/* if command 'K' is given the client writes to the socket and then terminates without waiting for an answer*/
 		if(ctemp == 'K' ){ 
 			printf("Client closing\n");
-			pthread_mutex_lock(&fifo);
-			wr_clean(torecv,&front);
-			pthread_mutex_unlock(&fifo);
-			pthread_exit(NULL);
+			pthread_cancel(pthread_self());
 		}
 	
 		if(ctemp == 'G'){
@@ -140,8 +185,12 @@ void * write_read ( void * arg){
 				}
 			}
 		}
+		pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED,&old_cancel_type); /*Guarantees that the thread isn't canceled while item is being freed, which would result in wr_clean trying to free item again*/
 		free(item);
+		item=NULL;
+		pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS,&old_cancel_type);		
 	}
+	pthread_cleanup_pop(0);
 }
 
 
@@ -191,6 +240,8 @@ int main(int argc, char *argv[])
     package * tosend;
     int end_operand = 0;
     int i;
+	struct sigaction sact;
+	sigset_t new_set;
     /*Flags*/
     int init = 0;
     int write_enable = 0;
@@ -198,15 +249,12 @@ int main(int argc, char *argv[])
     pthread_mutex_init(&fifo, NULL);
     create_fifo ( &front, &back);
     sem_init (&fifo_cnt, 0,0);
-    pthread_t wr_thread;
     int wr_arg;
 	/*Signals*/
 	sigset_t set;
-	sigfillset (&set);
-    sigprocmask(SIG_BLOCK, &set, NULL);
 	
-
-    pthread_t main_t;
+	wr_t=-1;
+    main_t=pthread_self();
     int old_cancel_type;
     wr_arg_t arg;
     main_clean_s aux_clean;
@@ -272,11 +320,21 @@ int main(int argc, char *argv[])
 	}
 
 	/* COMMUNICATION CYCLE */
+	sigfillset (&set);
+    sigprocmask(SIG_BLOCK, &set, NULL);
+
 	printf("To initialize communication session press 'I'\nTo terminate current session press 'K'\n");
     tosend = (package*) malloc(sizeof(package));
 	arg.wr_arg = sockfd;
-	arg.main=pthread_self();
-	pthread_create(&wr_thread, NULL, write_read, (void *) &arg);
+	pthread_create(&wr_t, NULL, write_read, (void *) &arg);
+	
+	sigemptyset(&sact.sa_mask);
+	sact.sa_flags = 0;
+	sact.sa_handler = sig_handler;
+	sigaction(SIGINT, &sact, NULL);
+	
+	block_except_sigint();	
+	
 	bzero(buffer,LEN+1);
 	while(fgets(buffer,LEN,file)!=NULL){
 		result=strtok((char*)buffer,delims);
@@ -287,13 +345,13 @@ int main(int argc, char *argv[])
 				if(sscanf(result,"%c%s", &ctemp, lixo)==1){
 					/*Client closes without waiting for server response*/
 					if(ctemp == 'K'){
-						/*if 'K' is inserted before the session is initialized the server must be warned. So the wr_thread must be created to perform the communication operations*/
-
+						/*if 'K' is inserted before the session is initialized the server must be warned. So the wr_t must be created to perform the communication operations*/
+						block_all_signals();
 						printf("Client closing\n");
 						tosend->op = 'K';
-						convert_send(0, tosend->data);
+						convert_send(0, tosend->data);				
 						send2fifo(tosend, end_operand);
-						pthread_join(wr_thread, NULL);
+						pthread_join(wr_t, NULL);
 						close(sockfd);
 						exit(0);
 					}
@@ -335,11 +393,13 @@ int main(int argc, char *argv[])
 							}
 							else if(ctemp == 'K')
 							{
+								block_all_signals();
 								printf("Client closing\n");
 								tosend->op = 'K';
 								convert_send(0, tosend->data);
 								send2fifo(tosend, end_operand);
-								pthread_join(wr_thread, NULL);
+								
+								pthread_join(wr_t, NULL);
 								close(sockfd);
 								exit(0);
 							}
@@ -365,19 +425,21 @@ int main(int argc, char *argv[])
 
 				}
 				if(write_enable == 1){
+					block_all_signals();					
 					send2fifo(tosend, end_operand);
+					block_except_sigint();
 				}
 			}
 			result = strtok(NULL, delims);
 		}
 		bzero(buffer,256);
     }
-    /* functions used specially when reading a file: all the data has been inputed however the server hasn't been able to respond accordingly, so a pthread_join has to be used. The end of the wr_thread is forced by sending a 'K'*/
-   
+    /* functions used specially when reading a file: all the data has been inputed however the server hasn't been able to respond accordingly, so a pthread_join has to be used. The end of the wr_t is forced by sending a 'K'*/
+	block_all_signals();
 	tosend->op = 'K';	
     convert_send(0, tosend->data);
 	send2fifo(tosend, end_operand);
-    pthread_join(wr_thread, NULL);
+    pthread_join(wr_t, NULL);
 
     close(sockfd);
     /* CLEANING */
